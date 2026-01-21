a35258e612694a37314f6f4ecfc71cc1b6226e2f
Rajat Agarwal <rajat.agarwal@infocusp.com>
2026-01-22 01:13:35 +0530

    added contract testing workflow

--- tests/contract/test_openapi_contract.py
+++ tests/contract/test_openapi_contract.py
@@ -1,6 +1,6 @@
 import schemathesis
 
-schema = schemathesis.openapi.from_uri("http://localhost:8000/openapi.json")
+schema = schemathesis.openapi.from_url("http://localhost:8000/openapi.json")
 
 @schema.parametrize()
 def test_api_contract(case):
